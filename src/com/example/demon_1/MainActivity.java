27a09e5e89b56b6379d9bb44d933497c666d1b9d
Carlos Espinoza <carlosarmikhael@gmail.com>
2014-03-13 13:46:09 -0430

    Agregando funciones al MainActivity

--- src/com/example/demon_1/MainActivity.java
+++ src/com/example/demon_1/MainActivity.java
@@ -8,14 +8,19 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
 import android.widget.Toast;
+import android.widget.ViewSwitcher.ViewFactory;
 
 public class MainActivity extends Activity {
 	
 	Button btnSearch;
 	Button btnOpenActivity;
+	LinearLayout inputControls;
 	public static final String TAG = MainActivity.class.toString();
 	
 	@Override
@@ -29,6 +34,26 @@ public class MainActivity extends Activity {
 		ButtonListener listener = new ButtonListener();
 		btnSearch.setOnClickListener(listener);
 		btnOpenActivity.setOnClickListener(listener);
+		
+		Button btnlist = new Button(this);
+		btnlist.setText(getString(R.string.btn_list));
+		btnlist.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, 
+				                                 LayoutParams.WRAP_CONTENT));
+		
+		LinearLayout mainContent = (LinearLayout)findViewById(R.id.mainContent);
+		
+		inputControls = (LinearLayout) View.inflate(this, 
+																 R.layout.input_controls_content, 
+																 null);
+		mainContent.addView(btnlist);
+		setInputControls();
+		mainContent.addView(inputControls);
+	}
+	
+	public void setInputControls(){
+		SeekBar seekbar = (SeekBar) inputControls.findViewById(R.id.seekBar1);
+		seekbar.setMax(10);
+		seekbar.setProgress(5);
 	}
 
 	@Override
